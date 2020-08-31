2541f6ce73c6167fdb7986d2ad63ffe66d39d7e7
Robert Koteles <koteles.roberto@gmail.com>
2020-08-31 12:19:19 +0100

    files added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# scriptloader
-General Script loader
+# General Script loader
+
+## About the code
+This snippet is for loading different scripts into the page. Use commas to separate scripts to load.
+
+## How to use
+
+```
+var l = new Loader();
+l.require(
+    [
+    "url_1", 
+    "url_2"
+    ], 
+function() {
+    log('All Scripts Loaded');
+});
+```
+
+
---
+++ src/script_loader.js
@@ -0,0 +1,45 @@
+/**
+ * Robert Koteles, Senior Web Developer, 2020
+ */
+
+// ==========================================================================
+// GENERAL SCRIPT LOADER
+// ==========================================================================
+var Loader = function(){}
+Loader.prototype = {
+    require: function (scripts, callback) {
+        this.loadCount      = 0;
+        this.totalRequired  = scripts.length;
+        this.callback       = callback;
+
+        for (var i = 0; i < scripts.length; i++) {
+            this.writeScript(scripts[i]);
+        }
+    },
+    loaded: function (evt) {
+        this.loadCount++;
+        if (this.loadCount == this.totalRequired && typeof this.callback == 'function') this.callback.call();
+    },
+    writeScript: function (src) {
+        var self = this;
+        var s = document.createElement('script');
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = src;
+        s.addEventListener('load', function (e) { self.loaded(e); }, false);
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(s);
+    }
+}
+
+
+
+var l = new Loader();
+l.require(
+    [
+    "https://polyfill.io/v3/polyfill.min.js?flags=gated&amp;features=es6", 
+    "https://xxxx.cloudfront.net/financeModule/main/main.js"
+    ], 
+function() {
+    log('All Scripts Loaded');
+});
